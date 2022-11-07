0f7cf53ec41456fc27ccb8ad5682a49233a2b23b
VishalKM24 <vkmandal2608@gmail.com>
2022-11-07 17:23:12 +0530

    Update Entity

--- Appointment.DAL/AppointmentDBContext.cs
+++ Appointment.DAL/AppointmentDBContext.cs
@@ -13,12 +13,12 @@ namespace Appointment.DAL
         }
 
         public DbSet<Common.Entity.Appointment> Appointments { get; set; }
-        public DbSet<Prescription> Prescriptions { get; set; }
-        public DbSet<Recommendation> Recommendations { get; set; }
-        public DbSet<Symptom> Symptoms { get; set; }
-        public DbSet<Vital> Vitals { get; set; }
-        public DbSet<Test> Tests { get; set; }
-        public DbSet<RecommendedClinic> RecommendedClinics { get; set; }
+        //public DbSet<Prescription> Prescriptions { get; set; }
+        //public DbSet<Recommendation> Recommendations { get; set; }
+        //public DbSet<Symptom> Symptoms { get; set; }
+        //public DbSet<Vital> Vitals { get; set; }
+        //public DbSet<Test> Tests { get; set; }
+        //public DbSet<RecommendedClinic> RecommendedClinics { get; set; }
 
     }
 }
