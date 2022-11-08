2b369b3b737c46c1a9573fa664d0495ab36735f5
Vachanamruth <gsvachan@gmail.com>
2022-11-08 15:28:08 +0530

    addedd IAppointmentRepo

---
+++ Appointment.DAL/IAppointmentRepo.cs
@@ -0,0 +1,25 @@
+﻿using Appointment.Common.DTO;
+using Appointment.common.Entities.Preset;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment.DAL
+{
+    public interface IAppointmentRepo
+    {
+        CardDetailsDTO getCardDetailsByDoctorID(int doctorID);
+        CardDetailsDTO getCardDetailsByPetID(int petID);
+        CardDetailsDTO getCardDetailsForBooking(int doctorID, DateTime date);
+        List<Test> getTests();
+        List<Clinic> getClinic();
+        List<Symptom> getSymptom();
+        List<PetIssue> getPetIssue();
+        List<Medicine> getMedicine();
+        common.Entities.Appointment getAppointment(int appointmentID);
+        common.Entities.Appointment editAppointment(int appointmentID, common.Entities.Appointment editedAppointment);
+        common.Entities.Appointment createAppointment(common.Entities.Appointment creatingAppointment);
+    }
+}
