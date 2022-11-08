1e587bf213a5682949f8c314b0004a3e3ed4e92a
Vachanamruth <gsvachan@gmail.com>
2022-11-08 11:12:59 +0530

    created businesslayer

---
+++ Appointment.BL/BusinessLayer.cs
@@ -0,0 +1,72 @@
+﻿using Appointment.common.Entities.Preset;
+using Appointment.Common.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment.BL
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        public common.Entities.Appointment createAppointment(CreatingAppointmentDTO creatingAppointment)
+        {
+            common.Entities.Appointment appointment = new common.Entities.Appointment();
+            appointment.PetID = creatingAppointment.PetID;
+            appointment.DoctorID = creatingAppointment.DoctorID;
+            appointment.AppointmentDate = creatingAppointment.AppoitmentDate;
+            appointment.AppointmentStatus = common.Entities.Status.Confirmed;
+        }
+
+        public common.Entities.Appointment editAppointment(int appointmentID, common.Entities.Appointment editedAppointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public common.Entities.Appointment getAppointment(int appointmentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardDetailsDTO getCardDetailsByDoctorID(int doctorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardDetailsDTO getCardDetailsByPetID(int petID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CardDetailsDTO getCardDetailsForBooking(int doctorID, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clinic> getClinic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medicine> getMedicine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PetIssue> getPetIssue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Symptom> getSymptom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Test> getTests()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
